951aaef46732f50dc29228062096cbe5552044a1
melinamontelongo <melinamontelongo97@gmail.com>
2023-01-14 17:14:46 -0500

    Added editable budget and currency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css"
 
 import { AppProvider } from './context/AppContext';
 import Budget from './components/Budget';
@@ -7,6 +9,7 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import Currency from "./components/Currency";
 
 const App = () => {
     return (
@@ -22,6 +25,9 @@ const App = () => {
                     </div>
                     <div className='col-sm'>
                         <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
                     </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,11 +2,30 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-
+    const { budget, expenses } = useContext(AppContext);
+    const { dispatch  } = useContext(AppContext);
+    const onChangeHandler = value => {
+        const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0)
+        if (value > 20000){
+            alert("Budget cannot exceed 20,000");
+        } else if (value < totalExpenses) {
+            alert("Budget cannot be lower than spending");
+        } else {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: value,
+            })
+        }
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <label>Budget: </label> 
+             <input 
+                type='number' 
+                value={budget}
+                onChange={(e) => onChangeHandler(e.target.value)}
+                step='10'>
+            </input>
         </div>
     );
 };

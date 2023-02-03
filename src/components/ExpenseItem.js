475b68b1fcc7466345ace22a7ff8755c0cb56e4d
littlefarro <115510939+littlefarro@users.noreply.github.com>
2023-02-03 13:38:42 -0500

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch, currency } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -25,26 +25,12 @@ const ExpenseItem = (props) => {
 
     }
 
-    const decreaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
-        dispatch({
-            type: 'RED_EXPENSE',
-            payload: expense
-        });
-
-    }
-
 
     return (
         <tr>
         <td>{props.name}</td>
-        <td>{currency.split(" ")[0]} {props.cost}</td>
-        <td><button className="btn-inc-dec btn-plus" onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><button className="btn-inc-dec btn-minus" onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        <td>£{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
